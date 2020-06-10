81876ac9b3fb3a75540678eddd7e794bb902e7dc
Markus <github@feyx.de>
2020-06-10 21:13:19 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,4 @@
 module.exports = {
-  purge: [
-    './src/**/*.html',
-    './src/**/*.svelte'
-  ],
-  purge: {
-    enabled: true,
-    content: [
-      './src/**/*.html',
-      './src/**/*.svelte'
-    ],
-  },
   theme: {},
   variants: {},
   plugins: [],
